947c9f0456d096f2bcda416b3e1a19a513297493
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2020-02-03 17:21:42 +0200

    finished and test passed

---
+++ src/__tests__/routes.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { server } = require('../server.js');
+const supergoose = require('@code-fellows/supergoose');
+
+const mockRequest = supergoose(server);
+const base64 = require('base-64');
+
+
+describe('Authentication Model',()=>{
+
+  it('POST to /signup to create a new user', ()=>{
+    let test = {'username': 'ahmadkhaleel96', 'password': 'Ahmad@$ 96 => INSTAcreative'};
+    mockRequest.post('/signup')
+      .send(test)
+      .then(data=>{
+        // console.log(data.text);
+        expect(data.text).toEqual(base64.encode(test.password));
+      });
+  });
+
+  // it('POST to /signin to get the user', ()=>{
+
+  // });
+
+  // it('GET to /users to get all users', ()=>{
+
+  // });
+});
